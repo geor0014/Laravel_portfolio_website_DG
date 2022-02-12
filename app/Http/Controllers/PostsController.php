3c17abb0bde141be10473147be37eb3e22aef66d
danielgeorgievl098 <geor0014@hz.nl>
2022-02-12 10:34:36 +0100

    fixing check errors

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -12,10 +12,9 @@ class PostsController extends Controller
 
         $posts = [1 => 'ict-fw', 2 => 'first-fb', 3 => 'prog-exp', 4 => 'swot-a', 5 => 'study-ch',];
 
-        if(!array_key_exists($post, $posts)) {
+        if (!array_key_exists($post, $posts)) {
             throw new NotFoundHttpException();
         }
         return view("blog-posts/$posts[$post]");
-
     }
 }
--- app/Models/Grade.php
+++ app/Models/Grade.php
@@ -1,32 +1,31 @@
 <?php
 
-    namespace App\Models;
+namespace App\Models;
 
-    use Illuminate\Database\Eloquent\Factories\HasFactory;
-    use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-    class Grade extends Model
+class Grade extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['course_name', 'test_name', 'lowest_passing_grade', 'best_grade',];
+
+    /**
+     * @param $num
+     * @return void
+     */
+    public function addResult($num)
     {
-        use HasFactory;
-
-        protected $fillable = [
-            'course_name',
-            'test_name',
-            'lowest_passing_grade',
-            'best_grade',
-        ];
-
-        public function addResult($num)
-        {
 
 
-            if($num > $this->best_grade) {
-                $this->best_grade = $num;
+        if ($num > $this->best_grade) {
+            $this->best_grade = $num;
 
-            }
-            if($this->lowest_passing_grade <= $this->best_grade) {
-                $this->passed_at = now();
-            }
-            $this->save();
         }
+        if ($this->lowest_passing_grade <= $this->best_grade) {
+            $this->passed_at = now();
+        }
+        $this->save();
     }
+}
