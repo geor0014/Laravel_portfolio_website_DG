68b8767e706b80640afdebc3ac9e84c3c02d6491
danielgeorgievl098 <geor0014@hz.nl>
2022-02-24 13:56:09 +0100

    Finish week 3: fix errors

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -9,26 +9,31 @@ class Course extends Model
 {
     use HasFactory;
 
-
-    public function grades(){
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function grades()
+    {
         return $this->hasMany(Grade::class);
     }
 
-    public function assignCredits(){
+    /**
+     * @return void
+     */
+    public function assignCredits()
+    {
         $grades = $this->grades->all();
 
         $avg = collect($grades)
-            ->filter(function($grade)
-            {
+            ->filter(function ($grade){
                 return $grade->best_grade >= 5.5;
             })
-            ->map(function($grade)
-            {
+            ->map(function ($grade){
                 return $grade->best_grade;
             })
             ->avg();
 
-        if($avg >= 5.5){
+        if ($avg >= 5.5) {
             $this->passed_at = now();
         }
             $this->save();
