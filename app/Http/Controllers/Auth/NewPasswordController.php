43f763fafe18f9d08d85387dcdf1861c3b38f95e
georgiev098 <Georgiev.Daniel@outlook.com>
2022-06-03 14:44:20 +0200

    add more additional password logic to NewPasswordController as well

--- app/Http/Controllers/Auth/NewPasswordController.php
+++ app/Http/Controllers/Auth/NewPasswordController.php
@@ -36,7 +36,7 @@ class NewPasswordController extends Controller
         $request->validate([
             'token' => ['required'],
             'email' => ['required', 'email'],
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'password' => ['required', 'confirmed', Password::min(9)->mixedCase()->numbers()->symbols()->uncompromised()],
         ]);
 
         // Here we will attempt to reset the user's password. If it is successful we
@@ -58,8 +58,8 @@ class NewPasswordController extends Controller
         // the application's home authenticated view. If there is an error we can
         // redirect them back to where they came from with their error message.
         return $status == Password::PASSWORD_RESET
-                    ? redirect()->route('login')->with('status', __($status))
-                    : back()->withInput($request->only('email'))
-                            ->withErrors(['email' => __($status)]);
+            ? redirect()->route('login')->with('status', __($status))
+            : back()->withInput($request->only('email'))
+            ->withErrors(['email' => __($status)]);
     }
 }
