cb01068929415d7c0ba0b5e5c3fa7e07378b7dcf
georgiev098 <Georgiev.Daniel@outlook.com>
2022-06-13 17:21:16 +0200

    update phpunit.xml for testing. update UserFactory so that password test passes. update AuthenticationTest so pass test passes. update PasswordConfirmationTest PasswordResetTest and RegistrationTest

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -20,10 +20,10 @@ class RegistrationTest extends TestCase
     public function test_new_users_can_register()
     {
         $response = $this->post('/register', [
-            'name' => 'Test User',
+            'name' => 'Test',
             'email' => 'test@example.com',
-            'password' => 'password',
-            'password_confirmation' => 'password',
+            'password' => 'Dani159753852?',
+            'password_confirmation' => 'Dani159753852?',
         ]);
 
         $this->assertAuthenticated();
